892e8fe80129387270226884650747a4c679d612
hakanozdmr <95864731+hakanozdmr@users.noreply.github.com>
2023-06-16 21:12:44 +0300

    first commit

---
+++ src/test/java/com/hakan/kodluyoruz/KodluyoruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakan.kodluyoruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
